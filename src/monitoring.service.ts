9eb06c93859635ccc93fca588cdd3a40adf9bd0c
cbosborn <chris.osborn@gmail.com>
2022-02-06 11:49:39 -0500

    Implement dialect welcome message.

--- src/monitoring.service.ts
+++ src/monitoring.service.ts
@@ -1,7 +1,31 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { DialectDataSource } from './dialect-data-source';
 import { MonitorFactory } from './monitor-factory';
-import { Monitors } from '@dialectlabs/monitor';
+import {
+  Monitors,
+  EventDetectionPipeline,
+  Operators,
+  PipeLogLevel,
+  SubscriberEvent,
+} from '@dialectlabs/monitor';
+
+export const welcomeMessagePipeline: EventDetectionPipeline<SubscriberEvent> = (
+  source,
+) =>
+  source
+    .pipe(Operators.Utility.log(PipeLogLevel.INFO))
+    .pipe(
+      Operators.Transform.filter(
+        ({ parameterData: { data } }) => data === 'added',
+      ),
+    )
+    .pipe(
+      Operators.Event.info(
+        'Welcome',
+        () =>
+          `Welcome to Dialect. This is an example of how you can receive notifications for events that happen on chain. In this case, the creation of a notification thread with Dialect.`,
+      ),
+    );
 
 @Injectable()
 export class MonitoringService implements OnModuleInit, OnModuleDestroy {
@@ -10,7 +34,12 @@ export class MonitoringService implements OnModuleInit, OnModuleDestroy {
     private readonly dialectDataSource: DialectDataSource,
   ) {}
 
-  onModuleInit() {}
+  onModuleInit() {
+    const monitor = this.monitorFactory.createSubscriberEventMonitor([
+      welcomeMessagePipeline,
+    ]);
+    monitor.start();
+  }
 
   async onModuleDestroy() {
     await Monitors.shutdown();
