a6b9df1107d9a02daeb0c7f8731e553d29a75c96
cbosborn <chris.osborn@gmail.com>
2022-03-22 12:21:34 -0400

    WIP on validating the server side auth headers.

--- src/wallet/wallet.middleware.ts
+++ src/wallet/wallet.middleware.ts
@@ -1,10 +1,10 @@
 import {
   HttpException,
   HttpStatus,
-  Inject,
   Injectable,
   NestMiddleware,
 } from '@nestjs/common';
+import { PublicKey } from '@solana/web3.js';
 import { NextFunction, Request, Response } from 'express';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -16,20 +16,39 @@ export class LoggerMiddleware implements NestMiddleware {
   }
 }
 
+// TODO: Consider using https://docs.nestjs.com/guards
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly prisma: PrismaService) {}
   async use(req: Request, res: Response, next: NextFunction) {
-    const public_key = req.params.public_key;
-    const wallet = await this.prisma.wallet.findUnique({
+
+    console.log('req.headers', req.headers);
+    let public_key: PublicKey;
+    try {
+      public_key = new PublicKey(req.params.public_key);
+    } catch (e: any) {
+      throw new HttpException(`Invalid format wallet public_key ${req.params.public_key}, please check your inputs and try again.`, HttpStatus.BAD_REQUEST);
+    }
+
+    const wallet = await this.prisma.wallet.upsert({
       where: {
-        publicKey: public_key,
+        publicKey: public_key.toBase58(),
       },
+      create: {
+        publicKey: public_key.toBase58(),
+      },
+      update: {},
     });
+
+    // const wallet = await this.prisma.wallet.findUnique({
+    //   where: {
+    //     publicKey: public_key.toBase58(),
+    //   },
+    // });
 
     if (!wallet)
       throw new HttpException(
-        `Invalid wallet public_key ${public_key}. Please check your inputs and try again.`,
+        `Invalid wallet public_key ${public_key.toBase58()}. Please check your inputs and try again.`,
         HttpStatus.UNAUTHORIZED,
       );
 
