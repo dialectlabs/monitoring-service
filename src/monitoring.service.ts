887d60063283b21fb02469dc07ca2398215d394a
GK <73589321+gklawcode@users.noreply.github.com>
2022-04-20 08:32:48 -1000

    Upgrade latest deps dialect and anchor 0.23 (#6)
    * upgraded deps to anchor 0.23.0 and latest dialect.
    
    * Add dialectThread notif sink for example sub added
    
    * monitor to 1.2.11

--- src/monitoring.service.ts
+++ src/monitoring.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { Monitors, Pipelines } from '@dialectlabs/monitor';
+import { Monitors, Pipelines, SubscriberState } from '@dialectlabs/monitor';
 import { DialectConnection } from './dialect-connection';
 
 @Injectable()
@@ -11,18 +11,18 @@ export class MonitoringService implements OnModuleInit, OnModuleDestroy {
       monitorKeypair: this.dialectConnection.getKeypair(),
       dialectProgram: this.dialectConnection.getProgram(),
     })
-      .subscriberEvents()
-      .transform({
-        keys: ['state'],
-        pipelines: [
-          Pipelines.notifyNewSubscribers({
-            messageBuilder: () =>
-              'Welcome to Dialect. This is an example of how you can receive notifications for events that happen on chain. In this case, the creation of a notification thread with Dialect.',
-          }),
-        ],
-      })
-      .dispatch('unicast')
-      .build();
+    .subscriberEvents()
+    .transform<SubscriberState, SubscriberState>({
+      keys: ['state'],
+      pipelines: [Pipelines.notifyNewSubscribers()],
+    })
+    .notify()
+    .dialectThread(({ value }) => ({
+      message: `Say hello to the future of web3 messaging.`,
+    }))
+    .and()
+    .dispatch('unicast')
+    .build();
     monitor.start();
   }
 
